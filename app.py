056dca6778237c4ab4f31d28d2fcff4b80591d7f
unknown <ynarula678@gmail.com>
2021-05-06 02:04:46 +0530

    db

--- app.py
+++ app.py
@@ -1,7 +1,19 @@
 from flask import Flask,render_template,flash, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
 from account import Registration, Login
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '85006f36b15fd56b8ccdcc455e8b6a04'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+db = SQLAlchemy(app)
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    image_file = db.Column(db.String(20), nullable=False, default='https://media.discordapp.net/attachments/741244174736556076/839414477920796702/icon.png')
+    password = db.Column(db.String(60), nullable=False)
+
+    def __repr__(self):
+        return f"User('{self.username}', '{self.email}', '{self.image_file}')"
 @app.route('/')
 def about():
     return render_template('about.html')
