5aa0e5d6f628ceb438459966acb104dde8985649
unknown <ynarula678@gmail.com>
2021-05-05 13:45:55 +0530

    html file

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
-from flask import Flask
+from flask import Flask,render_template
 app = Flask(__name__)
 @app.route('/')
 def hello():
-    return 'Hello world'
+    return render_template('layout.html')
 if __name__ == "__main__":
     app.run(debug=True)
