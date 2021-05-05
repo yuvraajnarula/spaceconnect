843cd85d05bfb015b8bd40fa8f44f3e1719701e6
unknown <ynarula678@gmail.com>
2021-05-05 23:42:04 +0530

    fix

--- app.py
+++ app.py
@@ -2,6 +2,12 @@ from flask import Flask,render_template
 app = Flask(__name__)
 @app.route('/')
 def hello():
-    return render_template('layout.html')
+    return render_template('about.html')
+@app.route('/login')
+def login():
+    return render_template('login.html')
+@app.route('/register')
+def register():
+    return render_template('register.html')
 if __name__ == "__main__":
     app.run(debug=True)
