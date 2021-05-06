aa65c3718df7d6cca70b1247fb54d9970bbeb2e2
unknown <ynarula678@gmail.com>
2021-05-06 12:05:31 +0530

    restructuring

---
+++ spaceconnect/model.py
@@ -0,0 +1,10 @@
+from spaceconnect import db
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    image_file = db.Column(db.String(20), nullable=False, default='https://media.discordapp.net/attachments/741244174736556076/839414477920796702/icon.png')
+    password = db.Column(db.String(60), nullable=False)
+
+    def __repr__(self):
+        return f"User('{self.username}', '{self.email}', '{self.image_file}')"
--- spaceconnect/routes.py
+++ spaceconnect/routes.py
@@ -1,19 +1,7 @@
 from flask import Flask,render_template,flash, redirect, url_for
-from flask_sqlalchemy import SQLAlchemy
-from account import Registration, Login
-app = Flask(__name__)
-app.config['SECRET_KEY'] = '85006f36b15fd56b8ccdcc455e8b6a04'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
-db = SQLAlchemy(app)
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(20), unique=True, nullable=False)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    image_file = db.Column(db.String(20), nullable=False, default='https://media.discordapp.net/attachments/741244174736556076/839414477920796702/icon.png')
-    password = db.Column(db.String(60), nullable=False)
-
-    def __repr__(self):
-        return f"User('{self.username}', '{self.email}', '{self.image_file}')"
+from spaceconnect import app
+from spaceconnect.account import Registration, Login
+from spaceconnect.model import User
 @app.route('/')
 def about():
     return render_template('about.html')
@@ -37,5 +25,3 @@ def register():
         flash(f'Account created for {form.username.data}. ', 'success')
         return redirect(url_for('home'))
     return render_template('register.html',form=form)
-if __name__ == "__main__":
-    app.run(debug=True)
