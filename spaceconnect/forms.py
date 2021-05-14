7fd1582024f0f28925aa9ff516de21f1bb775c4b
unknown <ynarula678@gmail.com>
2021-05-15 00:00:19 +0530

    aos

--- spaceconnect/forms.py
+++ spaceconnect/forms.py
@@ -1,5 +1,4 @@
 from flask_wtf import FlaskForm
-from flask_wtf.file import FileField, FileAllowed
 from flask_login import current_user
 from wtforms import StringField, PasswordField, SubmitField, BooleanField, TextAreaField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
@@ -38,4 +37,3 @@ class PostForm(FlaskForm):
 class adminForm(FlaskForm):
     password  = PasswordField('Pin', validators=[DataRequired()])
     submit = SubmitField('Submit')
-    
